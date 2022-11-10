f542a9d94b22e2c927747fe0934b3ded0521a3dd
Vadym Panchenko <vadympanchenko@gmail.com>
2022-11-10 02:51:25 +0200

    seventh

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 300,
+        adaptiveHieght: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevron-left-solid.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron-right-solid.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    arrows: false,
+                    dots: true
+                }
+            }
+        ]
+    });
+  });
