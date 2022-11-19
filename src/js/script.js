7a86d2cf2f14c94b21fbeda33bf0368d830d19db
Vadym Panchenko <vadympanchenko@gmail.com>
2022-11-19 13:12:08 +0200

    eleventh

--- src/js/script.js
+++ src/js/script.js
@@ -24,3 +24,29 @@
   document.querySelector('.next').addEventListener('click', function () {
     slider.goTo('next');
   });
+
+  (function($) {
+    $(function() {
+      
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+      
+    });
+
+
+    function toggleSlise(item){
+      $(item).each(function(i){
+        $(this).on('click', function(e){
+          e.preventDefault();
+          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
+        })
+      });
+    };
+
+    toggleSlise('.catalog-item__link');
+    toggleSlise('.catalog-item__back')
+  })(jQuery);
