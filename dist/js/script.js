1b1b4ea3107d17889aa201753ac4b2507e09e0cc
Vadym Panchenko <vadympanchenko@gmail.com>
2023-02-11 18:53:24 +0200

    last

---
+++ dist/js/script.js
@@ -0,0 +1,165 @@
+  const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    nav: false,
+    controls: false, 
+    mouseDrag: true,
+    responsive: {
+        992:{
+            nav: false,
+        },
+        300: {
+            nav: true,
+        }
+      }
+    }
+    );
+
+  document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+  });
+
+  document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+  });
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  (function($) {
+    wow = new WOW(
+      {
+      boxClass:     'wow',      // default
+      animateClass: 'animate__animated', // default
+      offset:       0,          // default
+      mobile:       true,       // default
+      live:         true        // default
+    }
+    )
+    wow.init();
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    $(function() {
+      
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+      
+    });
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    function toggleSlise(item){
+      $(item).each(function(i){
+        $(this).on('click', function(e){
+          e.preventDefault();
+          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
+        })
+      });
+    };
+
+    toggleSlise('.catalog-item__link');
+    toggleSlise('.catalog-item__back');
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //modal
+
+    $('[data-modal=consult]').on('click', function(){
+      $('.overlay, #consult').fadeIn('slow');
+    });
+
+    $('.modal__close').on('click', function(){
+      $('.overlay, #consult, #thanks, #order').fadeOut('slow');
+    });
+
+    
+
+    $('.button_mini').each(function(i){
+      $(this).on('click',function(){
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('slow');
+      })
+    });
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    function validateForms(form){
+      $(form).validate({
+        rules:{
+          fname:{
+            required: true,
+            minlength: 2
+          },
+          phone:"required",
+          email:{
+            required: true,
+            email: true
+          }
+        },
+        
+        messages: {
+          fname:{
+            required: "Пожалуйста, введите свое имя",
+            minlength: jQuery.validator.format("Введите {0} символова!")
+          },
+          phone: "Пожалуйста, введите свой номер телефона",
+          email: {
+            required: "Пожалуйста, введите свою почту",
+            email: "Неправельно введен адресс почты"
+          }
+        }
+  
+      });
+    };
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    validateForms('#consult-form');
+    validateForms('#consult form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+      e.preventDefault();
+      $.ajax({
+          type: "POST",
+          url: "mailer/smart.php",
+          data: $(this).serialize()
+      }).done(function() {
+          $(this).find("input").val("");
+          $('#consult, #order').fadeOut();
+          $('.overlay, #thanks').fadeIn('slow');
+
+          $('form').trigger('reset');
+      });
+      return false;
+    });
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Smooth scroll and pageup
+
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 1600) {
+          $('.pageup').fadeIn();
+      } else {
+          $('.pageup').fadeOut();
+      }
+    });
+
+    $("a[href^='#up']").click(function(){
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+    });
+
+  })(jQuery);
---
+++ dist/mailer/smart.php
@@ -0,0 +1,51 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'vadympanche@gmail.com';                 // Наш логин
+$mail->Password = 'X';                           // Наш пароль от ящика
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+ 
+$mail->setFrom('vadympanche@gmail.com', 'Pulse');   // От кого письмо 
+$mail->addAddress('vadympanchenko@gmail.com');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Данные';
+$mail->Body    = '
+		Пользователь оставил данные <br> 
+	Имя: ' . $name . ' <br>
+	Номер телефона: ' . $phone . '<br>
+	E-mail: ' . $email . '';
+
+if(!$mail->send()) {
+    return false;
+} else {
+    return true;
+}
+
+?>
